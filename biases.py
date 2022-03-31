8cb3d2506e6ec9c4ce2cbeae6e8534d2040d2428
Cheriti Y <yazid.cheriti@gmail.com>
2022-03-31 15:37:22 +0200

    test

---
+++ biases.py
@@ -0,0 +1,5 @@
+class biases :
+    def __init__(self) -> None:
+        self.mode = int(input("Quel est mon moyen de transport préféré pour réaliser mon trajet quotidien ? 1 pour voiture, 2 pour vélo, 3 pour la marche, 4 pour les transports en commun"))
+        
+        pass
