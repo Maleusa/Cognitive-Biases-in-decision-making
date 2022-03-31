f22bb5779ef3fa9bb8ce4b1dbebd5433a2be2845
ChloeConrad <90626378+ChloeConrad@users.noreply.github.com>
2022-03-31 16:14:28 +0200

    paramuser

--- user.py
+++ user.py
@@ -1,8 +1,24 @@
+from re import L
 from tkinter import DoubleVar
+from math import *
+
 
 
 class user:
-    def __init__(self) -> None:
-        self.mode = 
-        
-        pass
+
+    def __init__(self,) -> None:
+        preferenceCar = int(input("Quel est mon degré de préférence pour la voiture ? saisir un flotant de 1 à 4 où 1 est transport préféré et 4 est le transport le moins apprécié"))
+        preferenceBike = int(input("Quel est mon degré de préférence pour le vélo ? saisir un flotant de 1 à 4 où 1 est transport préféré et 4 est le transport le moins apprécié"))
+        preferenceBus = int(input("Quel est mon degré de préférence pour le bus ? saisir un flotant de 1 à 4 où 1 est transport préféré et 4 est le transport le moins apprécié"))
+        preferenceWalk = int(input("Quel est mon degré de préférence pour le bus ? saisir un flotant de 1 à 4 où 1 est transport préféré et 4 est le transport le moins apprécié"))
+        self.preference = list(("c",preferenceCar),("bu",preferenceBus),("bi",preferenceBike),("w",preferenceWalk))
+
+        self.gasPrice=float
+        self.subPrice=float
+        self.ratioCycleWay=float
+        self.busFrequency=int
+        self.busCapacity=int
+        self.carSpeed=int
+        self.bikeSpeed=int
+        self.walkSpeed=int
+        self.busSpeed=int
