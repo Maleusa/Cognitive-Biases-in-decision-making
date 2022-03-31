d9e089d12bb8c1c94f187344be9bbc0032afa2fb
ChloeConrad <90626378+ChloeConrad@users.noreply.github.com>
2022-03-31 15:07:45 +0200

    test

--- H.py
+++ H.py
@@ -1,3 +1,5 @@
 distance = float(input("Quelle distance en kilomètres sépare mon domicile et mon lieu de travail/étude ? "))
 preference = int(input("Quel est mon moyen de transport préféré pour réaliser mon trajet quotidien ? 1 pour voiture, 2 pour vélo, 3 pour la marche, 4 pour les transports en commun")) 
 biais = int(input("Quel biais module mon choix d'un moyen de transport ? 1 pour le biais de réactance, 2 pour le biais de sous additivité, etc..."))
+
+def durationCalculator():
