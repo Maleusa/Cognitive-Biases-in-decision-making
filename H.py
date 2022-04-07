d631151a80774e7f16d622bd830153a22c246957
ChloeConrad <90626378+ChloeConrad@users.noreply.github.com>
2022-04-07 14:30:23 +0200

    test

--- H.py
+++ H.py
@@ -4,14 +4,9 @@
 #biais = int(input("Quel biais module mon choix d'un moyen de transport ? 1 pour le biais de réactance, 2 pour le biais de sous additivité, etc..."))
 
 from biases import biases
+from user import *
 
 
-a = biases()
-print(a.mode)
-distance = float(input("Quelle distance en kilomètres sépare mon domicile et mon lieu de travail/étude ? "))
-preference = int(input("Quel est mon moyen de transport préféré pour réaliser mon trajet quotidien ? 1 pour voiture, 2 pour vélo, 3 pour la marche, 4 pour les transports en commun")) 
-biais = int(input("Quel biais module mon choix d'un moyen de transport ? 1 pour le biais de réactance, 2 pour le biais de sous additivité, etc..."))
 
-def durationCalculator():
-    return 3
+a = user()
 
