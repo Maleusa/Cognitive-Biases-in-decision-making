63f147e436ca3f83b10c0f1cea7bbd15ac140225
Cheriti Y <yazid.cheriti@gmail.com>
2022-04-07 13:44:31 +0200

    commit

--- user.py
+++ user.py
@@ -4,6 +4,19 @@ from switch import *
 
 from switch import *
 
+ECOLOGY = "ecology"
+COMFORT = "comfort"
+CHEAP = "cheap"
+SAFETY = "safety"
+PRATICITY = "praticity"
+FAST = "fast"
+CRITERIAS = [ECOLOGY, COMFORT, CHEAP, SAFETY, PRATICITY, FAST]
+
+HASBIKE = "agent has bike"
+HASCAR = "agent has car"
+HASBUS = "agent is on bus line"
+AGENTBOOLS = [HASBIKE, HASCAR, HASBUS]
+FITNESS = "agent's fitness level"
 
 
 class user:
@@ -15,10 +28,15 @@ class user:
         preferenceWalk = int(input("Quel est mon degré de préférence pour le bus ? saisir un flotant de 1 à 4 où 1 est transport préféré et 4 est le transport le moins apprécié"))
         self.preference = list(("c",preferenceCar),("bu",preferenceBus),("bi",preferenceBike),("w",preferenceWalk))
 
-
-        self.agent = generateAgent()
+        x = input("(r)andom agent priorities or (u)ser input or (f)ile input? : ")
+	    while x not in ["u","r","f"]:
+	        	x = input("(u)ser agent priorities or (r)andom ? : ")
+	    if x == "u": self = inputAgent()
+	    else if : self = generateAgent()
+        
 
         ## Préférence (ex ecologie confort cf switch python)
+
         ## Météo 
         ## Fitness level
         ## contraintes cf switch (sauf agenda)
