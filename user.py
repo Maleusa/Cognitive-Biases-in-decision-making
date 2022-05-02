aec05ef52397ed3a8e5c18f279516c3beb2718c3
Cheriti Y <yazid.cheriti@gmail.com>
2022-05-02 15:08:42 +0200

    fichier constant

--- user.py
+++ user.py
@@ -5,30 +5,8 @@ from re import A, L
 from math import *
 from typing import Iterable
 import random
+from constant import *
 
-# transportation modes
-BIKE = "bike"
-CAR = "car"
-BUS = "publictransport"
-WALK = "walk"
-LISTMODES = [BIKE,CAR,BUS,WALK]
-
-##Critères permettant le choix du moyen de transport
-ECOLOGY = "ecology"
-COMFORT = "comfort"
-CHEAP = "cheap"
-SAFETY = "safety"
-PRATICITY = "praticity"
-FAST = "fast"
-CRITERIAS = [ECOLOGY, COMFORT, CHEAP, SAFETY, PRATICITY, FAST]
-
-## Variables représentants les moyens de transports à disposition de l'agent
-HASBIKE = "agent has bike"
-HASCAR = "agent has car"
-HASBUS = "agent is on bus line"
-AGENTBOOLS = [HASBIKE, HASCAR, HASBUS]
-
-FITNESS = "agent's fitness level"
 
 
 class user:
