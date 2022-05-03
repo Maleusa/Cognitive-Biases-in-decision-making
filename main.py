997a8c946e6885b48103a51e71a41fd8d6851b97
ChloeConrad <90626378+ChloeConrad@users.noreply.github.com>
2022-05-03 11:53:18 +0200

    debug

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ envir=environnement
 actifAgent=user
 aCount=0
 eCount=0
+randomBool = False 
 while off != True:
   
     x= input("Do you want to create a new (e)nvironement,(m)anipulate the existing environement , manipulate or create an (a)gent, e(x)it the program ? : ")
@@ -17,7 +18,7 @@ while off != True:
         off=True
     if x=="e" :
         print("Creanting an environement")
-        envir=environnement()
+        envir=environnement(randomBool)
         eCount+=1
     if x=="a":
         if aCount==0 and eCount==0:
@@ -26,7 +27,7 @@ while off != True:
                 y=input("There is no actif environement, you can not create or manipulate agent without there environement, would you like to create one? (y)/(n) : ")
             if y=="y":
                 print("Creating an environement :")
-                envir=environnement()
+                envir=environnement(randomBool)
                 eCount+=1
             if y=="n":
                 print("Then there is nothing i can do for you, goodbye !")
@@ -38,7 +39,7 @@ while off != True:
                 y=input("There is no actif agent in the enviroment, would you like to create one ? (y)/(n) :")
             if y=="y":
                 print("Creating an agent :")
-                actifAgent=user(envir)
+                actifAgent=user(envir,randomBool)
                 aCount+=1
             if y=="n":
                 print("Then there is nothing i can do for you, goodbye !")
@@ -50,11 +51,11 @@ while off != True:
                 y=input("Would you like to create a new (a)gent or (m)anipulate the activ one ? : ")
             if y=="a":
                 print("Creating an agent :")
-                actifAgent=user(envir)
+                actifAgent=user(envir,randomBool)
                 aCount+=1
             if y=="m":
                 print("Manipulation is a bad habit quit it !") #done
-                actifAgent.biasedResults(envir)
+                actifAgent.biasedResults(envir,randomBool)
                 j=input("Would you like to save the results of the agent "+str(actifAgent.ident)+" ? (y)/(n) :")
                 while j not in ["y","n"]:
                     j=input("Would you like to save the results of the agent "+str(actifAgent.ident)+" ? (y)/(n) :")
@@ -67,7 +68,7 @@ while off != True:
                     y=input("You are trying to manipulate something that does not exist ! Would you like to create an environement ? (y)/(n) :")
                 if y=="y":
                     print("Creating an environement :")
-                    envir=environnement()
+                    envir=environnement(randomBool)
                     eCount+=1
                 else :
                     print("Then there is nothing i can do for you, goodbye ! ")
